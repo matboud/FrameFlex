45c10d366003e021ad53125b134e19e1621acfbc
Matboud <m.matboud@gmail.com>
2023-12-08 03:53:54 +0100

    config files

---
+++ frame-flex-frontend/lib/redux/slices/movieSlice/mockedData.js
@@ -0,0 +1,70 @@
+export const movies = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: ["Crime", "Drama"],
+    director: "Frank Darabont",
+    actors: ["Tim Robbins", "Morgan Freeman"],
+    rating: 9.3,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    director: "Christopher Nolan",
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
+    rating: 8.8,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    genre: ["Action", "Crime", "Drama"],
+    director: "Christopher Nolan",
+    actors: ["Christian Bale", "Heath Ledger"],
+    rating: 9.0,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    genre: ["Drama", "Romance"],
+    director: "Robert Zemeckis",
+    actors: ["Tom Hanks", "Robin Wright"],
+    rating: 8.8,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    genre: ["Crime", "Drama"],
+    director: "Quentin Tarantino",
+    actors: ["John Travolta", "Uma Thurman"],
+    rating: 8.9,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "Gladiator",
+    year: 2000,
+    genre: ["Action", "Drama", "Historical"],
+    director: "Ridley Scott",
+    actors: ["Russell Crowe", "Joaquin Phoenix"],
+    rating: 8.5,
+    imageUrl: "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    genre: ["Crime", "Drama"],
+    director: "Francis Ford Coppola",
+    actors: ["Marlon Brando", "Al Pacino"],
+    rating: 9.2,
+    imageUrl: "https://m.media-amazon.com/images/M/MV5BMTc1ODM5YjQtMmQzNS00Y2FkLWJhNTgtYTE5ZDY0NjQyNmRjXkEyXkFqcGdeQXVyMTY3ODE5NTY1._V1_.jpg",
+  },
+];
